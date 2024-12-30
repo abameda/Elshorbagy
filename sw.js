66c03bac77b1eaca180f5088cc4280e34c2d2ac5
abameda <69761302+abameda@users.noreply.github.com>
2024-12-30 05:57:41 +0200

    Add files via upload
    made it faster and more optmized for phones :P

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "v1";
+const assets = [
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/IMG_2438.jpeg",
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
